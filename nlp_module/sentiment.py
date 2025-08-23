e2d9b6d3e3ea764f0fdf14a8a91fcd69a7631481
Hertz Rafael <hertzrafael@hotmail.com>
2025-08-23 14:55:10 -0300

    fixing return of analysis sentiment method

--- nlp_module/sentiment.py
+++ nlp_module/sentiment.py
@@ -2,15 +2,7 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def analysis_sentiment(text: str):
-
     analyzer = SentimentIntensityAnalyzer()
     score = analyzer.polarity_scores(text)
-    compound = score['compound']
-
-    if compound >= 0.05:
-        return 'Positivo'
     
-    if compound <= -0.05:
-        return 'Negativo'
-    
-    return 'Neutro'
+    return score['compound']
