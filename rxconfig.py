202dc2f9350aa36009ecec6ecd8c5c4708bd82d3
iagonmic <iagoflavio_sa@hotmail.com>
2025-09-22 21:42:59 -0300

    base version

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="nltk_sentimental_analysis",
+    app_name="pulsenlp",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
