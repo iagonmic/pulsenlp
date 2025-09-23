3d1d29f4187e83b010af50172a57ac51a6dea6dd
iagonmic <iagoflavio_sa@hotmail.com>
2025-09-23 08:51:45 -0300

    Fixing value and input state

--- pulsenlp/pulsenlp.py
+++ pulsenlp/pulsenlp.py
@@ -2,8 +2,30 @@ import reflex as rx
 from rxconfig import config
 
 
-class State(rx.State):
-    """The app state."""
+class AppState(rx.State):
+    simulation_started: bool = False
+    topico: str = ""
+    num_users: int = 3
+
+    @rx.event
+    def set_topico(self, topico):
+        self.topico = topico
+
+    @rx.event
+    def set_num_users(self, num_users):
+        if num_users.isdigit():
+            num_users = int(num_users)
+        self.num_users = num_users
+
+    @rx.event
+    def start_simulation(self):
+        self.simulation_started = True
+        print(f"Simulação iniciada com tópico: {self.topico} e {self.num_users} agentes.")
+
+    @rx.event
+    def stop_simulation(self):
+        self.simulation_started = False
+        print("Simulação parada.")
 
 
 def index() -> rx.Component:
@@ -11,28 +33,32 @@ def index() -> rx.Component:
         rx.card(
             rx.vstack(
                 rx.text(
-                    "Defina o tópico e número de agentes",
+                    "Bem-vindo ao PulseNLP!",
                     align="center",
-                    size="5"
+                    size="5",
+                ),
+                rx.text(
+                    "Digite os valores abaixo para iniciar a simulação:",
+                    align="center",
+                    size="2"
                 ),
                 rx.hstack(
-                    rx.input(placeholder="Tópico", width="200px"),
-                    rx.input(placeholder="Número de agentes", width="200px"),
+                    rx.input(placeholder="Tópico da conversa", width="200px", on_change=AppState.set_topico, required=True),
+                    rx.input(placeholder="Quantidade de agentes", width="200px", on_change=AppState.set_num_users),
                     spacing="4",
                     align="center",
                     justify="center"
                 ),
-                rx.button("Iniciar", color_scheme="blue", width="100%"),
+                rx.button("Iniciar", color_scheme="blue", width="100%", on_click=AppState.start_simulation),
                 spacing="5",
                 align="center",
                 width="100%",  # garante que o vstack ocupe toda largura do card
-            ),
+                ),
             size="3",
-            padding="20px",
-        ),
-        padding="40px",
-    )
-
+            padding="20px",    
+            ),
+        padding="40px"
+        )
 
 
 app = rx.App()
